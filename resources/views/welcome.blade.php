57b82c9de6bae0b03bc336fb69824285caf57820
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-12-17 05:29:07 +0200

    hero style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -661,7 +661,9 @@
 
 @include('FrontEnd.Layouts.footer')
 
-<div id="preloader"></div>
+<div id="preloader">
+    <img src="{{asset('assets/img/Logo/ayma-solution.gif')}}">
+</div>
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 @include('FrontEnd.Layouts.foot')
