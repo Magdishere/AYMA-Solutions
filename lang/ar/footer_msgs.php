f02b742de3d80e321382bd1a887d7bc32df04776
Magd <magdelzalameh6@gmail.com>
2023-12-23 21:09:10 +0200

    footer translations

---
+++ lang/ar/footer_msgs.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+return [
+
+
+    'footer_useful_links' => 'روابط مفيدة',
+    'footer_home' => 'الرئيسية',
+    'footer_about_us' => 'من نحن',
+    'footer_services' => 'الخدمات',
+    'footer_terms_of_service' => 'شروط الخدمة',
+    'footer_privacy_policy' => 'سياسة الخصوصية',
+
+    'footer_our_services' => 'خدماتنا',
+    'footer_web_design' => 'تصميم الويب',
+    'footer_web_development' => 'تطوير الويب',
+    'footer_product_management' => 'إدارة المنتج',
+    'footer_marketing' => 'تسويق',
+    'footer_graphic_design' => 'تصميم جرافيك',
+
+    'footer_our_newsletter' => 'النشرة الإخبارية لدينا',
+    'footer_newsletter_description' => 'تامين كوام نولا كواي ليجام مولتوس أوت سينت كولبا ليجام نوستر ماغنا',
+    'footer_subscribe' => 'اشتراك',
+
+    'footer_copyright' => '&copy; حقوق الطبع والنشر <strong><span>AYMA Solutions</span></strong>. كل الحقوق محفوظة',
+    'footer_credits' => 'مصمم بواسطة <a href="https://www.linkedin.com/company/ayma-solutions/">AYMA Solutions</a>',
+];
---
+++ lang/en/footer_msgs.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+
+    'footer_useful_links' => 'Useful Links',
+    'footer_home' => 'Home',
+    'footer_about_us' => 'About us',
+    'footer_services' => 'Services',
+    'footer_terms_of_service' => 'Terms of service',
+    'footer_privacy_policy' => 'Privacy policy',
+
+    'footer_our_services' => 'Our Services',
+    'footer_web_design' => 'Web Design',
+    'footer_web_development' => 'Web Development',
+    'footer_product_management' => 'Product Management',
+    'footer_marketing' => 'Marketing',
+    'footer_graphic_design' => 'Graphic Design',
+
+    'footer_our_newsletter' => 'Our Newsletter',
+    'footer_newsletter_description' => 'Tamen quem nulla quae legam multos aute sint culpa legam noster magna',
+    'footer_subscribe' => 'Subscribe',
+
+    'footer_copyright' => '&copy; Copyright <strong><span>AYMA Solutions</span></strong>. All Rights Reserved',
+    'footer_credits' => 'Designed by <a href="https://www.linkedin.com/company/ayma-solutions/">AYMA Solutions</a>',
+];
